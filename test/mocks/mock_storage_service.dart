a9e91451e59cc4b47f5edf4e1f4c32897bf9d304
johan-axel <johan.axel@gmail.com>
2025-04-06 22:00:58 +0200

    enable settings in tests

--- test/mocks/mock_storage_service.dart
+++ test/mocks/mock_storage_service.dart
@@ -1,8 +1,12 @@
+import 'dart:async';
 import 'package:wine_taster/services/storage_service.dart';
 import 'package:wine_taster/models/tasting.dart';
+import 'package:wine_taster/models/settings.dart';
 
 class MockStorageService implements StorageService {
   final Map<int, Tasting> _mockStorage = {};
+  final Settings _settings = Settings();
+  final _settingsController = StreamController<Settings>.broadcast();
 
   @override
   Future<void> init() async {
@@ -32,4 +36,46 @@ class MockStorageService implements StorageService {
     _mockStorage.remove(id);
     print('[MockStorage] Tasting deleted successfully');
   }
+
+  @override
+  Future<void> dispose() async {
+    await _settingsController.close();
+  }
+
+  @override
+  String exportTastingsToCSV() {
+    return 'Mock CSV export';
+  }
+
+  @override
+  Settings getSettings() {
+    return _settings;
+  }
+
+  @override
+  Future<void> saveSettings(Settings settings) async {
+    _settings
+      ..minRating = settings.minRating
+      ..maxRating = settings.maxRating
+      ..ratingSteps = settings.ratingSteps
+      ..showWineType = settings.showWineType
+      ..showGrapes = settings.showGrapes
+      ..showCountry = settings.showCountry
+      ..showRegion = settings.showRegion
+      ..showProducer = settings.showProducer
+      ..showYear = settings.showYear
+      ..showColor = settings.showColor
+      ..showSmell = settings.showSmell
+      ..showSmellQuality = settings.showSmellQuality
+      ..showTaste = settings.showTaste
+      ..showTasteQuality = settings.showTasteQuality
+      ..showAftertaste = settings.showAftertaste
+      ..showAftertasteQuality = settings.showAftertasteQuality
+      ..showCharacteristics = settings.showCharacteristics
+      ..showRating = settings.showRating;
+    _settingsController.add(_settings);
+  }
+
+  @override
+  Stream<Settings> get settingsStream => _settingsController.stream;
 }
